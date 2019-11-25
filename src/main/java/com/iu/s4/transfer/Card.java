bbee8cdcf6c2be73f8b024013511cef0aee8ef14
Parkbb <pyj9088@gmail.com>
2019-11-25 14:26:09 +0900

    com

--- src/main/java/com/iu/s4/transfer/Card.java
+++ src/main/java/com/iu/s4/transfer/Card.java
@@ -1,13 +1,19 @@
 package com.iu.s4.transfer;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+@Component
+@Aspect
 public class Card {
 	
+	@After("execution(* com.iu.s4.transfer.Transfer.taxi())")
 	public void cash() {
 		System.out.println("==== 현금 내기 ====");
 	}
-	
+	@Around("execution(* com.iu.s4.transfer.Transfer.get*(*))")
 	public Object cardCheck(ProceedingJoinPoint join) {
 		System.out.println("==== 타기 전 카드 찍기====");
 		Object obj = null;
--- src/main/java/com/iu/s4/transfer/Work.java
+++ src/main/java/com/iu/s4/transfer/Work.java
@@ -16,5 +16,6 @@ public class Work {
 		
 		transfer.getSubway("KaKao");
 		
+		transfer.taxi();
 	}
 }
